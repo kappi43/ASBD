67949a4e307f5d807b22dc23a300d8d905e2888b
Kacper Czajkowski <s20145@pjwstk.edu.pl>
2022-03-06 13:45:28 +0100

    Committing the initial version of task 3

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var url = args[0];
+            var httpClient = new HttpClient();
+            var res = await httpClient.GetAsync(url);
+            var content = await res.Content.ReadAsStringAsync();
+            var regex = new Regex(@"[a-zA-Z0-9]+@([a-zA-Z]*\.)*[a-zA-Z]*");
+            var matches = regex.Matches(content);
+            foreach(var item in matches)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
